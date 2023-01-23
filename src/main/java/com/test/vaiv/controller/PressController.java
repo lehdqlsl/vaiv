1a67391ba677f7e4cc0c23e8b313e83345ca9277
dongbin <lehdqlsl@naver.com>
2023-01-24 00:43:40 +0900

    fix: Get Method 적용

--- src/main/java/com/test/vaiv/controller/PressController.java
+++ src/main/java/com/test/vaiv/controller/PressController.java
@@ -30,9 +30,9 @@ public class PressController {
         return "index";
     }
 
-    @PostMapping("/api/press")
+    @GetMapping("/api/press")
     @ResponseBody
-    public Map<String, Object> getPress(@RequestBody SearchDto searchDto) {
+    public Map<String, Object> getPress(@ModelAttribute @Valid SearchDto searchDto) {
         HashMap<String, Object> ret = new HashMap<>();
         List<PressDto> pressList = pressService.findPressByOption(searchDto);
         ret.put("press", pressList);
@@ -47,18 +47,21 @@ public class PressController {
     }
 
     private String getTitle(SearchDto searchDto) {
-        if (!searchDto.getName().equals("")) {
+        if (!searchDto.getName().isEmpty()) {
             return String.format("%s 기자회견 타임라인", searchDto.getName());
-        } else if (!searchDto.getParty().equals("")) {
+        }
+        if (!searchDto.getParty().isEmpty()) {
             return String.format("%s 기자회견 타임라인", searchDto.getParty());
-        } else if (searchDto.getStartDate() != null && searchDto.getEndDate() != null) {
+        }
+        if (searchDto.getStartDate() != null && searchDto.getEndDate() != null) {
             return String.format("%s ~ %s 타임라인", searchDto.getStartDate(), searchDto.getEndDate());
-        } else if (searchDto.getStartDate() != null) {
+        }
+        if (searchDto.getStartDate() != null) {
             return String.format("%s 이후 타임라인", searchDto.getStartDate());
-        } else if (searchDto.getEndDate() != null) {
+        }
+        if (searchDto.getEndDate() != null) {
             return String.format("%s 이전 타임라인", searchDto.getStartDate());
         }
         return "전체 타임라인";
     }
-
 }
